351981b6676e90eec98154601caa2773e522ac62
Richard <richard@wambiz.com>
2016-06-08 22:06:19 +0100

    unsaved changes

--- zavit.Infrastructure.Places/PublicPlacesApis/GooglePlacesSearchResult.cs
+++ zavit.Infrastructure.Places/PublicPlacesApis/GooglePlacesSearchResult.cs
@@ -9,6 +9,18 @@ namespace zavit.Infrastructure.Places.PublicPlacesApis
 
     public class GooglePlace
     {
+        public GoogleGeometry geometry { get; set; }
         public string place_id { get; set; }
+    }
+
+    public class GoogleGeometry
+    {
+        public GoogleLocation location { get; set; }
+    }
+
+    public class GoogleLocation
+    {
+        public double lat { get; set; }
+        public double lng { get; set; }
     }
 }
