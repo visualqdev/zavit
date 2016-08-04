9c2c38d6c0ac837af3a15ba9562cb3d6e29df8b1
Richard <richard@wambiz.com>
2016-08-04 08:48:41 +0100

    started adding routing to mvc

--- zavit.Web.Mvc/App/app.js
+++ zavit.Web.Mvc/App/app.js
@@ -2,28 +2,7 @@
 
 (function() {
 
-    var DEFAULT_HASH = 'home';
-
-    crossroads.addRoute('/',() => { loadNames() });
-    crossroads.addRoute('about', () => { loadAddress()});
-
-    //crossroads.routed.add(console.log, console); //log all routes
-
-    //only required if you want to set a default value
-    //if(! hasher.getHash()){
-    //    hasher.setHash(DEFAULT_HASH);
-    //}
-
-    function parseHash(newHash, oldHash){
-        // second parameter of crossroads.parse() is the "defaultArguments" and should be an array
-        // so we ignore the "oldHash" argument to avoid issues.
-        crossroads.parse(newHash);
-    }
-
-    hasher.initialized.add(parseHash); //parse initial hash
-    hasher.changed.add(parseHash); //parse hash changes
-
-    hasher.init(); //start listening for hash changes
+    
 
 
     function capitalise(word) {
