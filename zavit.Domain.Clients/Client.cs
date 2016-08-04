58edb2169f1ee5822b6345045549639aff572f6e
Richard <richard@wambiz.com>
2016-08-04 20:59:22 +0100

    started adding refresh tokens for authorization

---
+++ zavit.Domain.Clients/Client.cs
@@ -0,0 +1,15 @@
+﻿using zavit.Domain.Shared;
+
+namespace zavit.Domain.Clients
+{
+    public class Client : IEntity<string>
+    {
+        public string Id { get; set; }
+        public string Secret { get; set; }
+        public string Name { get; set; }
+        public ApplicationTypes ApplicationType { get; set; }
+        public bool Active { get; set; }
+        public int RefreshTokenLifeTime { get; set; }
+        public string AllowedOrigin { get; set; }
+    }
+}
