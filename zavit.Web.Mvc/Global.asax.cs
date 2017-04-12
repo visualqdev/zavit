0ba35920b9ee495b185de4a2557176ad92cee757
Richard <richard@wambiz.com>
2017-04-12 23:31:59 +0100

    ts and cs

--- zavit.Web.Mvc/Global.asax.cs
+++ zavit.Web.Mvc/Global.asax.cs
@@ -1,22 +1,28 @@
 ﻿using System;
+using System.Configuration;
 using System.Web;
-using System.Web.Http;
-using System.Web.Mvc;
-using System.Web.Routing;
-using zavit.Domain.Shared;
 using zavit.Infrastructure.Ioc;
 using zavit.Infrastructure.Logging;
-using zavit.Web.Api;
-using zavit.Web.Mvc.IocConfiguration;
 
 namespace zavit.Web.Mvc
 {
     public class Global : HttpApplication
     {
+        static bool _useHttps;
+
         protected void Application_Start(object sender, EventArgs e)
         {
+            bool.TryParse(ConfigurationManager.AppSettings["Application.UseHttps"], out _useHttps);
         }
 
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            if (!Context.Request.IsSecureConnection && _useHttps && !Context.Response.IsRequestBeingRedirected)
+            {
+                Response.Redirect(Context.Request.Url.ToString().Replace("http:", "https:"));
+            }
+        }
+        
         protected void Application_Error(Object sender, EventArgs e)
         {
             var raisedException = Server.GetLastError();
