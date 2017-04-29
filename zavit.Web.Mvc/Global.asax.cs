981c0e74a81e0697057d717d31d867430963a832
Richard <richard@wambiz.com>
2017-04-29 16:05:42 +0100

    fixed https

--- zavit.Web.Mvc/Global.asax.cs
+++ zavit.Web.Mvc/Global.asax.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Configuration;
 using System.Web;
 using zavit.Infrastructure.Ioc;
 using zavit.Infrastructure.Logging;
@@ -8,32 +7,13 @@ namespace zavit.Web.Mvc
 {
     public class Global : HttpApplication
     {
-        static bool _useHttps;
-
         protected void Application_Start(object sender, EventArgs e)
-        {
-            bool.TryParse(ConfigurationManager.AppSettings["Application.UseHttps"], out _useHttps);
-        }
+        {}
 
         protected void Application_BeginRequest(object sender, EventArgs e)
-        {
-            if (_useHttps)
-            {
-                switch (Request.Url.Scheme)
-                {
-                    case "https":
-                        Response.AddHeader("Strict-Transport-Security", "max-age=300");
-                        break;
-                    case "http":
-                        var path = "https://" + Request.Url.Host + Request.Url.PathAndQuery;
-                        Response.Status = "301 Moved Permanently";
-                        Response.AddHeader("Location", path);
-                        break;
-                }
-            }
-        }
+        {}
         
-        protected void Application_Error(Object sender, EventArgs e)
+        protected void Application_Error(object sender, EventArgs e)
         {
             var raisedException = Server.GetLastError();
             var loggerFactory = Container.Instance.Resolve<ILoggerFactory>();
