eaad573e87c8b4aeb9b79f6f3f72503244b028c2
Richard <richard@wambiz.com>
2017-06-18 22:22:49 +0100

    landing page

---
+++ zavit.Web.Mvc/Controllers/AboutController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace zavit.Web.Mvc.Controllers
+{
+    public class AboutController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
